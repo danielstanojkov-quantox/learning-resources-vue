8cbd76295aa61849e154bdb5acb4f392f1b369e1
Daniel Stanojkov <daniel.stanojkov@quantox.com>
2021-06-13 15:55:30 +0200

    First components & props

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official VueJs documentation',
+          link: 'https://v3.vuejs.org/'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
